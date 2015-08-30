76bc34dcfab60ae0878c22205a2cb221618d42c2
AVINASH <avinash.kumar@taslc.com>
2015-08-30 20:31:57 +0530

    Source & campaign changes: add referal and direct source

--- app/code/local/Custom/Common/Helper/Data.php
+++ app/code/local/Custom/Common/Helper/Data.php
@@ -514,7 +514,9 @@ class Custom_Common_Helper_Data extends Mage_Core_Helper_Abstract{
 	 
 	 public function checkUtmzscCookies(){
 		$getData = Mage::app()->getRequest()->getParams();
-		$utm_source = isset($getData['utm_source']) ? $getData['utm_source'] : '';
+		$_refererUrl = Mage::app()->getRequest()->getServer('HTTP_REFERER');
+		$_refererDomain = str_ireplace('www.', '', parse_url($_refererUrl, PHP_URL_HOST));
+		$utm_source = isset($getData['utm_source']) ? $getData['utm_source'] : (isset($_refererDomain) ? $_refererDomain : '(direct)');
 		$utm_medium = isset($getData['utm_medium']) ? $getData['utm_medium'] : '';
 		$utm_campaign = isset($getData['utm_campaign']) ? $getData['utm_campaign'] : '';
 
